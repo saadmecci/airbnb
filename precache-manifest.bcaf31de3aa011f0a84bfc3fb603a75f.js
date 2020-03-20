c4f084b998697fb54e693352bd3a0d1b5ef9e515
Saad Mecci <saadmecci@yahoo.com>
2020-03-20 10:28:53 -0500

    Updates

---
+++ precache-manifest.bcaf31de3aa011f0a84bfc3fb603a75f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eae7418267e40707116fb3eec4562088",
+    "url": "/airbnb/index.html"
+  },
+  {
+    "revision": "4ac3e0f35f468ef28883",
+    "url": "/airbnb/static/css/2.c51b6406.chunk.css"
+  },
+  {
+    "revision": "48bc56299d8cbbe52449",
+    "url": "/airbnb/static/css/main.0ebc32e9.chunk.css"
+  },
+  {
+    "revision": "4ac3e0f35f468ef28883",
+    "url": "/airbnb/static/js/2.996bddb5.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/airbnb/static/js/2.996bddb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48bc56299d8cbbe52449",
+    "url": "/airbnb/static/js/main.ee3c9e6b.chunk.js"
+  },
+  {
+    "revision": "42906cf901f23920282f",
+    "url": "/airbnb/static/js/runtime-main.de5bf7a3.js"
+  },
+  {
+    "revision": "11415c4d508ee6c1ed716b697b213081",
+    "url": "/airbnb/static/media/AirbnbCerealBlack.11415c4d.woff"
+  },
+  {
+    "revision": "6021dfe8dec0b7e6578d6acb473c0121",
+    "url": "/airbnb/static/media/AirbnbCerealBold.6021dfe8.woff"
+  },
+  {
+    "revision": "886339361d3ff50e8a95ac5cdc63a82e",
+    "url": "/airbnb/static/media/AirbnbCerealBook.88633936.woff"
+  },
+  {
+    "revision": "eef0aa3fd067658b3ae410fd1b563aa0",
+    "url": "/airbnb/static/media/AirbnbCerealExtraBold.eef0aa3f.woff"
+  },
+  {
+    "revision": "d2a616df336ba4fbc0bafbf4683da4b1",
+    "url": "/airbnb/static/media/AirbnbCerealLight.d2a616df.woff"
+  },
+  {
+    "revision": "220b1dd31cbf1b11825433158d644def",
+    "url": "/airbnb/static/media/AirbnbCerealMedium.220b1dd3.woff"
+  },
+  {
+    "revision": "ec023b0e441ffd0ff3a2627469051a5a",
+    "url": "/airbnb/static/media/airbnb_logo.ec023b0e.svg"
+  },
+  {
+    "revision": "8a841593a56f88a0bf1ebfeb4a4af9a9",
+    "url": "/airbnb/static/media/dashboardPicture.8a841593.jpg"
+  }
+]);
